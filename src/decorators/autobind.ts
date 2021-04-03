dd0a4864c332891b5c30bee87291fa32783e69c2
raf0nx <69021954+raf0nx@users.noreply.github.com>
2021-04-04 01:08:10 +0200

    refactor: Split code with ES modules

---
+++ src/decorators/autobind.ts
@@ -0,0 +1,14 @@
+// autobind decorator
+export function Autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
+  const originalMethod = descriptor.value;
+  const adjDescriptor: PropertyDescriptor = {
+    enumerable: false,
+    configurable: true,
+    get() {
+      const boundFn = originalMethod.bind(this);
+      return boundFn;
+    },
+  };
+
+  return adjDescriptor;
+}
